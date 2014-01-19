150169a52a636d0a41398a23c510ff46243cb918
Christine Wang <cwang@okta.com>
2014-01-19 10:37:30 -0800

    Initial Commit

---
+++ CPTempCalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CPTempCalculator
+//
+//  Created by Christine Wang on 1/19/14.
+//  Copyright (c) 2014 Christine Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
